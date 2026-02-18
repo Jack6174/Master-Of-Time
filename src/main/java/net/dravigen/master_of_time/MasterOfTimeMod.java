32ee65f2fb11ddbe8f4bf1ba756ef08388b3062c
DraViGen <fireshadoxe@gmail.com>
2026-02-18 15:43:55 +0100

    fix deleted class causing crash

---
+++ src/main/java/net/dravigen/master_of_time/MasterOfTimeMod.java
@@ -0,0 +1,10 @@
+package net.dravigen.master_of_time;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MasterOfTimeMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	
+	}
+}
